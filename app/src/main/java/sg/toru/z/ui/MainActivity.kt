48ba0eb2ad2f0ba739bbd6793b0cca3a5217d31e
Toru Choi <blueocean239@gmail.com>
2020-03-06 01:19:31 +0800

    additional configurations for map

--- app/src/main/java/sg/toru/z/ui/MainActivity.kt
+++ app/src/main/java/sg/toru/z/ui/MainActivity.kt
@@ -2,12 +2,34 @@ package sg.toru.z.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.maps.*
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.LatLngBounds
 import sg.toru.z.R
+import sg.toru.z.util.Utils
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var map:GoogleMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initializeMap()
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
+        map.setMinZoomPreference(10F)
+        map.setMaxZoomPreference(16F)
+        map.uiSettings.isCompassEnabled = true
+        map.uiSettings.isZoomGesturesEnabled = true
+        map.setLatLngBoundsForCameraTarget(LatLngBounds(LatLng(Utils.SOUTHMOST_LAT, Utils.WESTMOST_LNG), LatLng(Utils.NORTHMOST_LAT, Utils.EASTMOST_LNG)))
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(LatLng(Utils.MARINA_BAY_SANDS_LAT, Utils.MARINA_BAY_SANDS_LNG),11F))
+    }
+
+    private fun initializeMap(){
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.mapFragment) as SupportMapFragment
+        mapFragment.getMapAsync(this)
     }
 }
---
+++ app/src/main/java/sg/toru/z/util/Utils.kt
@@ -0,0 +1,20 @@
+package sg.toru.z.util
+
+object Utils {
+    /* INFORMATION OF TERRITORY OF SINGAPORE */
+    const val NORTHMOST_LAT = 1.470556
+    const val NORTHMOST_LNG = 103.817222
+
+    const val WESTMOST_LAT = 1.242538
+    const val WESTMOST_LNG = 103.6047383
+
+    const val SOUTHMOST_LAT = 1.238450
+    const val SOUTHMOST_LNG = 103.832928
+
+    const val EASTMOST_LAT = 1.349264
+    const val EASTMOST_LNG = 104.043313
+
+    /* INFORMATION OF DEFAULT LOCATION WHEN USER IS NOT IN SINGAPORE */
+    const val MARINA_BAY_SANDS_LAT = 1.282302
+    const val MARINA_BAY_SANDS_LNG = 103.858528
+}
