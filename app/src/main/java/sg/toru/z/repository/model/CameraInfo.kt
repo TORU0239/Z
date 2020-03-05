1866f320bedeb44b588bda20f2c8d0eb61368302
Toru Choi <blueocean239@gmail.com>
2020-03-06 00:32:48 +0800

    add data classes used for camera information

---
+++ app/src/main/java/sg/toru/z/repository/model/CameraInfo.kt
@@ -0,0 +1,23 @@
+package sg.toru.z.repository.model
+
+data class CameraInfoItems(
+    val timeStamp: String,
+    val cameras: List<CameraInfo>
+)
+data class CameraInfo (
+    val timeStamp: String,
+    val image: String,
+    val cameraId: String,
+    val location: CameraLocation
+)
+
+data class CameraLocation(
+    val latitude: Double,
+    val longitude: Double
+)
+
+data class CameraImageMetaData(
+    val height: String,
+    val width: String,
+    val md5: String
+)
